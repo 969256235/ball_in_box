42db60b701c6f3b4540af1d42ef2cf0cd2874c6f
Sagen <32996212+969256235@users.noreply.github.com>
2018-06-03 14:23:18 +0800

    Update ballinbox.py

--- ball_in_box/ballinbox.py
+++ ball_in_box/ballinbox.py
@@ -4,12 +4,16 @@ from math import cos
 import random
 import importlib
 
+from .validate import validate
+from .validate import intersect
+
 __all__ = ['ball_in_box']
 
 global t
 # dynamicly load
 global turtle
 # whether display detail process
+# DISPLAY_RESULT = True
 DISPLAY_RESULT = False
 # VIEW_DETAIL = True
 VIEW_DETAIL = False
@@ -26,56 +30,6 @@ def load_turtle():
     except:
         DISPLAY_RESULT = False
         VIEW_DETAIL = False
-
-def intersect(circle : tuple, circles : list, blockers : list):
-    """
-    Args:
-        circle: next a circle to check whether could be in current box
-        circles: circles which have been put in box
-        blockers: blockers
-    Return:
-        A list of circle in the circles which overlap with arg circle
-    """
-    inters = []
-    # Does circle intersect with the box?
-    xmr = circle[0] - circle[2]
-    xpr = circle[0] + circle[2]
-    ymr = circle[1] - circle[2]
-    ypr = circle[1] + circle[2]
-
-    if xmr < -1:
-        inters.append((1, 0, -1))
-    if xpr > 1:
-        inters.append((-1, 0, -1))
-    if ymr < -1:
-        inters.append((0, 1, -1))
-    if ypr > 1:
-        inters.append((0, -1, -1))
-        
-    # Does circle intersect with blockers?
-    if blockers is not None and len(blockers) > 0:
-        for block in blockers:
-            x = circle[0]
-            y = circle[1]
-            r = circle[2]
-            bx = block[0]
-            by = block[1]
-            if math.sqrt((x - bx)**2 + (y - by)**2) < r:
-                inters.append((bx, by, 0))
-
-    # Does circle intersect with other circles?
-    for circle1 in circles:
-        x1 = circle1[0]
-        y1 = circle1[1]
-        r1 = circle1[2]
-        x = circle[0]
-        y = circle[1]
-        r = circle[2]  
-        if math.sqrt((x1 - x)**2 + (y1 - y)**2) < (r1 + r):
-            inters.append(circle1)
-
-    # Intersect with nothing
-    return inters
 
 def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
     """
@@ -125,68 +79,15 @@ def find_step_max(blockers : list, circles : list):
     Return:
         next circle which can be put in current box
     """
-
-
-
-    circle = None
-    while circle is None: 
-        x = random.random()*2 - 1
-        y = random.random()*2 - 1
-        circle = find_max_circle_at(x, y, blockers, circles)
+    count = 200
+    max_circle = (-1, -1, 0)
+    for x in range(count):
+        for y in range(count):
+            circle = find_max_circle_at(x/count*2-1, y/count*2-1, blockers, circles)
+            if circle is not None and circle[2] > max_circle[2]:
+                max_circle = circle
     
-    # 速度衰减速率
-    coll_v_rate = 0.98
-    # 初始速度
-    velocity = vec(0.7, 0.7)
-
-    while True:
-        inters = intersect((circle[0], circle[1], circle[2]), circles, blockers)
-        while len(inters) is 0:
-            circle = find_max_circle_at(circle[0], circle[1], blockers, circles)
-            circle = (circle[0], circle[1], circle[2]+EPSILON*100)
-            inters = intersect((circle[0], circle[1], circle[2]), circles, blockers)
-
-        # norm - 接触面法向
-        if inters[0][2] is -1:
-            norm = vec(inters[0][0], inters[0][1])
-        else: 
-            norm = vec(circle[0]-inters[0][0], circle[1]-inters[0][1])
-
-        dot_a = norm.dot(velocity) / (norm.magnitude()*velocity.magnitude())
-        if dot_a < 0:
-            if abs(dot_a) < abs(cos(math.pi/9*4)):
-                velocity = norm.normal()*velocity.magnitude()
-            velocity = velocity*-coll_v_rate
-            velocity.mirror(norm)
-        
-        if velocity.magnitude() < 0.1:
-            while len(inters) > 0:
-                circle = (circle[0], circle[1], circle[2]*0.99)
-                inters = intersect((circle[0], circle[1], circle[2]), circles, blockers)
-            return circle
-
-        #print("Velocity is %f"%(velocity.magnitude()))
-        circle = (circle[0] + velocity.x * 0.05,circle[1] + velocity.y * 0.05, circle[2])
-
-        ###############################################
-        # view detail
-        if VIEW_DETAIL:
-            if random.random() > 0.4:
-                continue
-            global t
-
-            t.color("black")
-            t.penup()
-            t.setposition(circle[0]*150, circle[1]*150-circle[2]*150)
-            t.pendown()
-            t.circle(circle[2]*150)
-
-            t.color("white")
-            t.penup()
-            t.setposition(circle[0]*150, circle[1]*150-circle[2]*150)
-            t.pendown()
-            t.circle(circle[2]*150)
-        ###############################################
+    return max_circle
 
 def find_max_circle_at(x : float, y : float, blockers : list, circles : list):
     """
